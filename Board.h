60f84daea9b910a47642c8e5d07254571fea24d2
smchanda2244 <smchanda2244@gmail.com>
2026-04-03 11:42:00 -0400

    headers

--- Board.h
+++ Board.h
@@ -1,1 +1,14 @@
 #pragma once
+#include "Types.h"
+
+namespace Engine {
+	class Board {
+	private:
+		Coordinate m_black_pieces;
+		Coordinate m_white_pieces;
+
+		bool Player;
+
+
+	};
+}
--- Constants.h
+++ Constants.h
@@ -2,5 +2,6 @@
 #include "Types.h"
 
 namespace Engine {
-	static constexpr Coordinate 
+	static constexpr Coordinate MAX_POSITION = 63;
+	static constexpr Coordinate MIN_POSITION = 0;
 }
